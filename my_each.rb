8338d830aac97f2ae1c265ec4a791d8f4ac13ac8
Joe Quattrone <joequattrone5@gmail.com>
2018-10-19 12:41:47 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,5 @@
 def my_each(array)
+  if block_given?
   i = 0
   
   while i < array.length
@@ -6,4 +7,7 @@ def my_each(array)
     i = i + 1
   end
   array
+  else 
+     "Hey! No block was given!"
+  end
 end
